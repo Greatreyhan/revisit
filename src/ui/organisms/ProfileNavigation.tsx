5ceca266a3ead34b1b3442292b5454c15271a7db
greatreyhan <greatreyhan@students.undip.ac.id>
2025-03-14 14:55:05 +0700

    update navigation

--- src/ui/organisms/ProfileNavigation.tsx
+++ src/ui/organisms/ProfileNavigation.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMdApps } from "react-icons/io";
-import { MdInsertPageBreak, MdLocationPin, MdOutlineSecurity } from "react-icons/md";
+import { MdInsertPageBreak, MdLocationPin, MdOutlineMenu, MdOutlineSecurity } from "react-icons/md";
 import { MdOutlineLogout } from "react-icons/md";
 import { Logo } from '../../assets/icons';
 import { useFirebase } from '../../utils/FirebaseContext'; // Update the path as needed
@@ -9,6 +9,7 @@ import { IoCalendarSharp } from 'react-icons/io5';
 
 const ProfileNavigation: React.FC = () => {
     const { signOut } = useFirebase();
+    const [showNav, setShowNav] = useState(false)
 
     const handleLogout = async (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
@@ -25,9 +26,45 @@ const ProfileNavigation: React.FC = () => {
     return (
         <div className='md:w-2/12 md:h-screen bg-slate-100 flex flex-col justify-between py-8 fixed left-0 top-0'>
             {/* Logo */}
-            <div className='flex justify-center'>
+            <div className='md:flex hidden justify-center'>
                 <img className='w-32' src={Logo} alt="Logo" />
             </div>
+
+            {/* Small Navigation */}
+            <div className='flex md:hidden w-full fixed bg-slate-200 top-0 justify-start'>
+                <MdOutlineMenu className='text-5xl p-2 m-1 cursor-pointer' onClick={()=>setShowNav(!showNav)} />
+            </div>
+
+            {/* Navigation Mini */}
+            <div className={`fixed h-screen bg-black bg-opacity-40 top-14 ${showNav ? "w-screen flex flex-col md:hidden":"w-0 hidden"}`}>
+                <div className={`pt-16  gap-3 bg-slate-100  text-gray-800 ${showNav ? "w-8/12":"w-0"}  h-screen`}>
+                    <a href='/profile' className='cursor-pointer hover:font-semibold hover:text-primary-dark text-sm px-6 py-2 flex items-center'>
+                        <IoMdApps className='text-2xl mr-1' />
+                        <span>Dashboard</span>
+                    </a>
+                    <a href='/report' className='cursor-pointer hover:font-semibold hover:text-primary-dark text-sm px-6 py-2 flex items-center'>
+                        <MdInsertPageBreak className='text-2xl mr-1' />
+                        <span>Report</span>
+                    </a>
+                    <a href='/visit' className='cursor-pointer hover:font-semibold hover:text-primary-dark text-sm px-6 py-2 flex items-center'>
+                        <MdLocationPin className='text-2xl mr-1' />
+                        <span>Visit</span>
+                    </a>
+                    <a href='/assistant' className='cursor-pointer hover:font-semibold hover:text-primary-dark text-sm px-6 py-2 flex items-center'>
+                        <RiCustomerServiceFill className='text-2xl mr-1' />
+                        <span>Assistant</span>
+                    </a>
+                    <a href='/schedule' className='cursor-pointer hover:font-semibold hover:text-primary-dark text-sm px-6 py-2 flex items-center'>
+                        <IoCalendarSharp className='text-2xl mr-1' />
+                        <span>Schedule</span>
+                    </a>
+                    <a href='/warranty' className='cursor-pointer hover:font-semibold hover:text-primary-dark text-sm px-6 py-2 flex items-center'>
+                        <MdOutlineSecurity className='text-2xl mr-1' />
+                        <span>Warranty</span>
+                    </a>
+                </div>
+            </div>
+
 
             {/* List Menu */}
             <div className='pt-16 md:flex hidden text-left flex-col text-gray-800 flex-1'>
